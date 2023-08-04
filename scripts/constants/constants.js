8183fa52b760c8fe1c5bd1a244334a66ad13b94c
Diego Calvo <100451091@alumnos.uc3m.es>
2023-08-04 15:45:36 +0200

    feat(constants): created js file to store app constants

---
+++ scripts/constants/constants.js
@@ -0,0 +1,291 @@
+const spanishDeck = [
+    {
+      name: {
+        en: "Ace of gold",
+        es: "As de oros",
+      },
+      image: "spanishDeck/ace-gold.png",
+    },
+    {
+      name: {
+        en: "Two of gold",
+        es: "Dos de oros",
+      },
+      image: "spanishDeck/two-gold.png",
+    },
+    {
+      name: {
+        en: "Three of gold",
+        es: "Tres de oros",
+      },
+      image: "spanishDeck/three-gold.png",
+    },
+    {
+      name: {
+        en: "Four of gold",
+        es: "Cuatro de oros",
+      },
+      image: "spanishDeck/four-gold.png",
+    },
+    {
+      name: {
+        en: "Five of gold",
+        es: "Cinco de oros",
+      },
+      image: "spanishDeck/five-gold.png",
+    },
+    {
+      name: {
+        en: "Six of gold",
+        es: "Seis de oros",
+      },
+      image: "spanishDeck/six-gold.png",
+    },
+    {
+      name: {
+        en: "Seven of gold",
+        es: "Siete de oros",
+      },
+      image: "spanishDeck/seven-gold.png",
+    },
+    {
+      name: {
+        en: "Jack of gold",
+        es: "Sota de oros",
+      },
+      image: "spanishDeck/jack-gold.png",
+    },
+    {
+      name: {
+        en: "Knight of gold",
+        es: "Caballo de oros",
+      },
+      image: "spanishDeck/knight-gold.png",
+    },
+    {
+      name: {
+        en: "King of gold",
+        es: "Rey de oros",
+      },
+      image: "spanishDeck/king-gold.png",
+    },
+    {
+      name: {
+        en: "Ace of cups",
+        es: "As de copas",
+      },
+      image: "spanishDeck/ace-cups.png",
+    },
+    {
+      name: {
+        en: "Two of cups",
+        es: "Dos de copas",
+      },
+      image: "spanishDeck/two-cups.png",
+    },
+    {
+      name: {
+        en: "Three of cups",
+        es: "Tres de copas",
+      },
+      image: "spanishDeck/three-cups.png",
+    },
+    {
+      name: {
+        en: "Four of cups",
+        es: "Cuatro de copas",
+      },
+      image: "spanishDeck/four-cups.png",
+    },
+    {
+      name: {
+        en: "Five of cups",
+        es: "Cinco de copas",
+      },
+      image: "spanishDeck/five-cups.png",
+    },
+    {
+      name: {
+        en: "Six of cups",
+        es: "Seis de copas",
+      },
+      image: "spanishDeck/six-cups.png",
+    },
+    {
+      name: {
+        en: "Seven of cups",
+        es: "Siete de copas",
+      },
+      image: "spanishDeck/seven-cups.png",
+    },
+    {
+      name: {
+        en: "Jack of cups",
+        es: "Sota de copas",
+      },
+      image: "spanishDeck/jack-cups.png",
+    },
+    {
+      name: {
+        en: "Knight of cups",
+        es: "Caballo de copas",
+      },
+      image: "spanishDeck/knight-cups.png",
+    },
+    {
+      name: {
+        en: "King of cups",
+        es: "Rey de copas",
+      },
+      image: "spanishDeck/king-cups.png",
+    },
+    {
+      name: {
+        en: "Ace of swords",
+        es: "As de espadas",
+      },
+      image: "spanishDeck/ace-swords.png",
+    },
+    {
+      name: {
+        en: "Two of swords",
+        es: "Dos de espadas",
+      },
+      image: "spanishDeck/two-swords.png",
+    },
+    {
+      name: {
+        en: "Three of swords",
+        es: "Tres de espadas",
+      },
+      image: "spanishDeck/three-swords.png",
+    },
+    {
+      name: {
+        en: "Four of swords",
+        es: "Cuatro de espadas",
+      },
+      image: "spanishDeck/four-swords.png",
+    },
+    {
+      name: {
+        en: "Five of swords",
+        es: "Cinco de espadas",
+      },
+      image: "spanishDeck/five-swords.png",
+    },
+    {
+      name: {
+        en: "Six of swords",
+        es: "Seis de espadas",
+      },
+      image: "spanishDeck/six-swords.png",
+    },
+    {
+      name: {
+        en: "Seven of swords",
+        es: "Siete de espadas",
+      },
+      image: "spanishDeck/seven-swords.png",
+    },
+    {
+      name: {
+        en: "Jack of swords",
+        es: "Sota de espadas",
+      },
+      image: "spanishDeck/jack-swords.png",
+    },
+    {
+      name: {
+        en: "Knight of swords",
+        es: "Caballo de espadas",
+      },
+      image: "spanishDeck/knight-swords.png",
+    },
+    {
+      name: {
+        en: "King of swords",
+        es: "Rey de espadas",
+      },
+      image: "spanishDeck/king-swords.png",
+    },
+    {
+      name: {
+        en: "Ace of clubs",
+        es: "As de bastos",
+      },
+      image: "spanishDeck/ace-clubs.png",
+    },
+    {
+      name: {
+        en: "Two of clubs",
+        es: "Dos de bastos",
+      },
+      image: "spanishDeck/two-clubs.png",
+    },
+    {
+      name: {
+        en: "Three of clubs",
+        es: "Tres de bastos",
+      },
+      image: "spanishDeck/three-clubs.png",
+    },
+    {
+      name: {
+        en: "Four of clubs",
+        es: "Cuatro de bastos",
+      },
+      image: "spanishDeck/four-clubs.png",
+    },
+    {
+      name: {
+        en: "Five of clubs",
+        es: "Cinco de bastos",
+      },
+      image: "spanishDeck/five-clubs.png",
+    },
+    {
+      name: {
+        en: "Six of clubs",
+        es: "Seis de bastos",
+      },
+      image: "spanishDeck/six-clubs.png",
+    },
+    {
+      name: {
+        en: "Seven of clubs",
+        es: "Siete de bastos",
+      },
+      image: "spanishDeck/seven-clubs.png",
+    },
+    {
+      name: {
+        en: "Jack of clubs",
+        es: "Sota de bastos",
+      },
+      image: "spanishDeck/jack-clubs.png",
+    },
+    {
+      name: {
+        en: "Knight of clubs",
+        es: "Caballo de bastos",
+      },
+      image: "spanishDeck/knight-clubs.png",
+    },
+    {
+      name: {
+        en: "King of clubs",
+        es: "Rey de bastos",
+      },
+      image: "spanishDeck/king-clubs.png",
+    },
+  ];
+  
+const decksDict = {
+    spanish: {
+      list: spanishDeck,
+      source: "spanishDeck",
+    },
+  };
+
+export {decksDict}
